abca5a6a28c1ac08d6b13aa806ef736de2312f40
simon-ichec <31247816+simon-ichec@users.noreply.github.com>
2022-04-06 00:06:57 +0100

    Update sw-script.js

--- sw-script.js
+++ sw-script.js
@@ -10,7 +10,7 @@ if (!website) {
 
 // default main style sheet
 //var main_style_href = website + "/" + "css/hpc-event-style.css";
-var main_style_href = "https://simon-ichec.github.io/test-coe/css/hpc-event-style.css";
+var main_style_href = "https://fs.hlrs.de/projects/par/events/2022/test/css/hpc-event-style.css";
 
 // Set the country if specified
 const restrict_country = hpc_events_element.hasAttribute("data-country") ? hpc_events_element.getAttribute("data-country") : null;
